078671ef0a8fcb55bf97928d49ffec459745aab0
RafalKodland <168269766+RafalKodland@users.noreply.github.com>
2024-05-18 13:10:23 +0200

    Zmiana programu na bot discrod

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-meme_dict = {
-            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
-            "LOL": "Częsta reakcja na coś zabawnego",
-            }
+import discord
+from discord.ext import commands
 
-word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
+# Zmienna intencje przechowuje uprawnienia bota
+intents = discord.Intents.default()
+# Włączanie uprawnienia do czytania wiadomości
+intents.message_content = True
+# Tworzenie bota w zmiennej klienta i przekazanie mu uprawnień
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-if word in meme_dict.keys():
-    # Co powinniśmy zrobić, jeśli słowo zostało znalezione?
-    print(meme_dict[word])
-else:
-    # Co powinniśmy zrobić, jeśli słowo nie zostało znalezione?
-    print("Niestety nie mamy takiej definicji!")
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Cześć")
+
+@bot.command()
+async def bye(ctx, id = 0):
+    await ctx.send(bot.get_emoji(id).name)
+
+@bot.command()
+async def echo(ctx, messages):
+    await ctx.send(messages)
+
+bot.run("MTIzODgwMDY2NDI5Nzk5NjMwOA.GUi-jl.d1TDk2rodmQBiJTOf4obznKcTFNTfPPYzsONGA")
